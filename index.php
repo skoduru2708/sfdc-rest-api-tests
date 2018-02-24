a4639c10e8ac6c1157ee8589db3623f3594a8d29
Srinivas Koduru <skoduru2708@gmail.com>
2018-02-24 11:57:05 -0800

    make it better

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+if (!isset($access_token)) {
+    $access_token = null;
+}
+
+if (is_null($access_token)) {
+    $access_token = getAccessTokenFromSFDC();
+}
+
+function getAccessTokenFromSFDC() {
+    $sfdc_token_request_endpoint = "https://login.salesforce.com/services/oauth2/token";
+
+    $data = array(
+        'grant_type' => 'password',
+        'client_id' => '3MVG9i1HRpGLXp.qkF8N41ZyYwD2K8qDZgKzti6Y3gqcghklQcwi0tFwAg33kx6rh1jIzIa8GfqzFyMqqv8L9',
+        'client_secret' => '1957267389927066618',
+        'username' => 'skoduru2708@gmail.com.lightning',
+        'password' => 'Sairam@12aXxNAoWKGb87pBHbZdu0fuit'
+    );
+    
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $sfdc_token_request_endpoint);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    $output = curl_exec($ch);
+    $info = curl_getinfo($ch);
+    curl_close($ch);
+    
+    print_r($output);
+}
+
+?>
